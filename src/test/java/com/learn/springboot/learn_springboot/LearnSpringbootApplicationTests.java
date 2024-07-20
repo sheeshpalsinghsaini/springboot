d9a516cfa413070d90b46cc612b7de2f18a49c34
Sheeshpal Singh <123sheeshpalsinghsaini@gmail.com>
2024-07-20 17:09:02 +0530

    springboot project created

---
+++ src/test/java/com/learn/springboot/learn_springboot/LearnSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learn.springboot.learn_springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
